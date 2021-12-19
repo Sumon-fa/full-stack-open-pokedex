c1db3a3b95cf1d92185004e8550c7212bb7b11fb
Sumon Fakir <fakirsumon78@gmail.com>
2021-12-19 20:39:00 +0200

    testing

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,47 @@
 module.exports = {
-  env: {
-    node: true,
-    browser: true,
-    es6: true,
-    "jest/globals": true,
+  "env": {
+    "node": true,
+    "browser": true,
+    "es6": true,
+    "jest/globals": true
   },
-  extends: ["eslint:recommended", "plugin:react/recommended"],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended"
+  ],
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
     },
-    ecmaVersion: 2018,
-    sourceType: "module",
+    "ecmaVersion": 2018,
+    "sourceType": "module"
   },
-  plugins: ["react", "jest"],
-  rules: {
-    indent: ["error", 2],
-    "linebreak-style": [
+  "plugins": [
+    "react", "jest"
+  ],
+  "rules": {
+    "indent": [
       "error",
-      process.platform === "win32" ? "windows" : "unix",
+        2
+      ],
+    'linebreak-style': ['error', process.platform === 'win32' ? 'windows' : 'unix'],
+    "quotes": [
+      "error",
+      "single"
     ],
-    quotes: ["error", "single"],
-    semi: [2, "always"],
-    eqeqeq: "error",
+    "semi": [
+      2,
+      "always"
+    ],
+    "eqeqeq": "error",
     "no-trailing-spaces": "error",
-    "object-curly-spacing": ["error", "always"],
-    "arrow-spacing": ["error", { before: true, after: true }],
+    "object-curly-spacing": [
+      "error", "always"
+    ],
+    "arrow-spacing": [
+      "error", { "before": true, "after": true }
+    ],
     "no-console": 0,
-    "react/prop-types": 0,
-  },
-};
+    "react/prop-types": 0
+  }
+}
